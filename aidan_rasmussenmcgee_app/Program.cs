f86dc52100efcfe456d2c34a2585a57fce45342f
Aidan Rasmussen McGee <aidan.rasmussenmcgee@umconnect.umt.edu>
2023-09-01 11:42:00 -0600

    tha Assignment

---
+++ aidan_rasmussenmcgee_app/Program.cs
@@ -0,0 +1,32 @@
+﻿// This is the tha assignment for BMIS 365 
+
+// Below I've copied the code from the assignment per the instructions
+//Added a single line at the very top of the code with my first and last name
+// Added the missing three semicolons to the string, int, and third WriteLine
+//Added a note to the string name that it represents a mascot
+//Added a note to the int to that it represents the number of mascots
+//Created white space between the variables at the top of the code. Created white space between the int and ("The weather...). Created white space between ("The weather...) and if variable.
+//Corrected spellings mistakes by capitlizing the l in Writeline and the w in writeline.
+//Created indentation once in both the if and else statements.
+//Right above the if statement I inserted a multiline comment block that contained the date, current time, a copy of text from the assignment, and my feelings about the Griz.
+
+// Aidan Rasmussen McGee
+string name = "Griz"; //This indicates a mascot
+
+int quantity = 1; //This indicates a count for the number of mascots
+
+Console.WriteLine("The weather is phenomenal in Montana.");    
+
+/*
+9/01/2023 11:38 AM
+This code assesses the number of mascots.
+Yeah baby, go Griz!
+*/
+    if (quantity == 1)
+{
+Console.WriteLine("We have just 1 Griz");
+}
+    else
+{
+Console.WriteLine("Sorry. We don't have a Griz.");
+}
